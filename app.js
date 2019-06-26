4cdc0fee49b28c691b6f606a67c286145422789c
Aiden <owlcapone0@gmail.com>
2019-06-26 12:33:40 -0400

    Updating webpage to work with server

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ app.set("view engine", "ejs"); //setting up templating engine
 app.get('/about', function(req, res){
     res.render('about.ejs');
 })
+app.get('/contact', function(req, res){
+    res.render('contact.ejs');
+})
+app.get('/ForHonor', function(req, res){
+    res.render('ForHonor.ejs');
+})
+app.get('/portfolio', function(req, res){
+    res.render('portfolio.ejs');
+})
+app.get('/anime', function(req, res){
+    res.render('anime.ejs');
+})
+
+app.use(express.static('./public'));
 
 app.listen(3000, function(err){
     if (err){console.log(err);
